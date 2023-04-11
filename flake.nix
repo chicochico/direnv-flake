a18fe80577acc5c8de36d4e3d79d868f275c8b41
Francisco Chiang <mail@fchiang.com>
2023-04-11 23:18:47 +0200

    Add example of installing older version of terraform

--- flake.nix
+++ flake.nix
@@ -1,9 +1,14 @@
 {
   description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    terraform = {
+      url = "github:NixOS/nixpkgs/69fcff00d0fbf7d742f31900f46a5c27c5caf85e"; # 1.2.9
+    };
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, terraform }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -16,6 +21,9 @@
             # pip is not needed since we declare dependencies in this 
             # flake.nix, it can be added however with python311Packages.pip
             python311Packages.requests
+          ];
+          buildInputs = [
+            terraform.legacyPackages.${system}.terraform
           ];
           shellHook = ''
             export FOO=foo
